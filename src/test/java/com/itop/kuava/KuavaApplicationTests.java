f7d13d64b8e8f7db191f272a4fcc385700ab5d96
ss_962026658@126.com <ss_962026658@126.com>
2020-05-06 13:49:25 +0800

    1

---
+++ src/test/java/com/itop/kuava/KuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itop.kuava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
